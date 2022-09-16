267dfcec6452acb7504620d81674d278df57e737
Charlotte GOSSELIN <cgosseli@e1r4p5.clusters.42paris.fr>
2022-09-16 17:43:54 +0200

    added color checking

--- src/parsing/create_plane.c
+++ src/parsing/create_plane.c
@@ -6,7 +6,7 @@
 /*   By: cgosseli <cgosseli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 17:39:18 by cgosseli          #+#    #+#             */
-/*   Updated: 2022/09/15 17:52:40 by cgosseli         ###   ########.fr       */
+/*   Updated: 2022/09/16 17:00:19 by cgosseli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,9 @@ void	create_plane(char *data, t_object *plane)
 	plane->center = parse_position(data + i, &i);
 	plane->direction = parse_direction(data + i, &i);
 	plane->color = parse_position(data + i, &i);
+	if (check_valid_color_range(plane->color))
+	{
+		ft_putstr_fd("Something is wrong with the plane color\n", 2);
+		ft_memory(0, 0);
+	}
 }
