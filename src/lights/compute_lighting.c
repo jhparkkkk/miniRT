5246686841a30ce3328a62e54e0ba095c2b96ef4
Charlotte GOSSELIN <cgosseli@e1r2p15.clusters.42paris.fr>
2022-09-28 14:09:40 +0200

    color rgb clamping corrected, pb when red sp, white light

--- src/lights/compute_lighting.c
+++ src/lights/compute_lighting.c
@@ -6,12 +6,14 @@
 /*   By: cgosseli <cgosseli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 16:39:13 by cgosseli          #+#    #+#             */
-/*   Updated: 2022/09/27 15:50:38 by cgosseli         ###   ########.fr       */
+/*   Updated: 2022/09/27 16:38:20 by cgosseli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_rt.h"
 
+/* Calculates the intensity of light at the impact of *ray on the *sphere.
+Returns the intensity. */
 double	compute_lighting(t_ray *ray, t_object *sp, t_world *world)
 {
 	t_vec3 point;
@@ -29,7 +31,7 @@ double	compute_lighting(t_ray *ray, t_object *sp, t_world *world)
 	vec_light = vec_substract(world->light.position, point);
 	n_dot_l = vec_dot(normal, vec_light);
 	if (n_dot_l > 0.0)
-		intensity += world->light.intensity * n_dot_l / (vec_len(normal) * vec_len(vec_light));
+		intensity += world->light.intensity * (n_dot_l / (vec_len(normal) * vec_len(vec_light)));
 	
 	// /* COLOR LIGHTING */
 	// world->light.intensity_r = vec_dot(normal, vec_light) * world->ambient_light.intensity * 0.5 * 0 + intensity * 0.5 * 0;
@@ -52,8 +54,8 @@ double	compute_lighting(t_ray *ray, t_object *sp, t_world *world)
 		view = vec_scalar(ray->direction, -1.0);
 		r_dot_v = vec_dot(reflect, view);
 		if (r_dot_v > 0.0)
-			// intensity += (world->light.intensity * r_dot_v * sp->specular_exponent) / (vec_len(reflect) * vec_len(view));
-			intensity += world->light.intensity * pow(r_dot_v / (vec_len(reflect) * vec_len(view)), sp->specular_exponent);
+			// intensity += (world->light.intensity * (r_dot_v / (vec_len(reflect) * vec_len(view)))* sp->specular_exponent) ;
+			intensity += world->light.intensity * pow(r_dot_v / (vec_len(reflect) * vec_len(view)), 1 / sp->specular_exponent);
 	}
 	return (intensity);
 }
