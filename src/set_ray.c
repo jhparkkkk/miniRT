b5372fa1cfbe3d6fc410fecde33a08f9c636923d
Jee hyun PARK <jeepark@e1r2p16.clusters.42paris.fr>
2022-10-01 13:52:36 +0200

    fixing hit_sp

--- src/set_ray.c
+++ src/set_ray.c
@@ -6,7 +6,7 @@
 /*   By: jeepark <jeepark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 13:16:54 by jeepark           #+#    #+#             */
-/*   Updated: 2022/09/26 11:55:12 by jeepark          ###   ########.fr       */
+/*   Updated: 2022/10/01 13:22:23 by jeepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	set_ray(t_ray *ray, t_cam cam, int i, int j)
 	ray->origin = cam.position;
 	ray->direction.x = j - SIZEX / 2;
 	ray->direction.y = i - SIZEY / 2;
-	ray->direction.z = -10.0;
-	ray->direction = vec_add(ray->direction, cam.direction);
+	ray->direction.z = 10.0;
+	ray->direction = vec_add(ray->origin, ray->direction);
 
 }
