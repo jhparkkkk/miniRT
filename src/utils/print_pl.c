0a0b4f83480cb91b356db84614a14b218b96d653
Charlotte GOSSELIN <cgosseli@e1r2p15.clusters.42paris.fr>
2022-10-18 14:55:44 +0200

    init plane

---
+++ src/utils/print_pl.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_pl.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgosseli <cgosseli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 14:32:41 by cgosseli          #+#    #+#             */
+/*   Updated: 2022/10/18 14:34:42 by cgosseli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mini_rt.h"
+
+void	print_pl(t_object pl)
+{
+	printf("\nplane position : x : %f, y : %f, z : %f\n", pl.center.x,
+	pl.center.y, pl.center.z);
+
+	printf("plane orientation : x : %f, y : %f, z : %f\n", pl.direction.x,
+	pl.direction.y, pl.direction.z);
+
+	printf("plane color : %f, y : %f, z : %f\n", pl.color.x,
+	pl.color.y, pl.color.z);
+	printf("\n");
+}
